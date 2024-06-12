e09bdaf563edd5c40af1935df648d70be55b392a
Crazy Loks <adrianmendez2001@gmail.com>
2024-06-11 22:38:11 -0600

    Fin del proyecto - actualización del repositorio remoto

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv';
 dotenv.config(); // llamamos a las variables de entorno
 
-
+//! console aqui cambiamos de .ts a .js para generar la carpeta de produccion, ahi solo existen archivos js
 const db = new Sequelize(process.env.DATABASE_URL!, { // conectamos los modelos
-    models: [__dirname + '/../models/**/*.ts' ], // __dirname regresa la ubicacion del archivo que manda llamar esta funcion
+    models: [__dirname + '/../models/**/*.js' ], // __dirname regresa la ubicacion del archivo que manda llamar esta funcion
     logging: false
 });
 
